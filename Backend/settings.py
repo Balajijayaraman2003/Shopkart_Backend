1991914f7c2964842c1ce181ddf87574348bed9d
Balajijayaraman2003 <jayabalaji2003kdm@gmail.com>
2026-03-08 14:07:30 +0530

    fix django deploy

--- Backend/settings.py
+++ Backend/settings.py
@@ -226,9 +226,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
